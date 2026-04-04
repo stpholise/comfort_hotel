18f6a75f39e8fb01ee74b0d2d4a717db4dcbb34e
stephen olise <stpholise@gmail.com>
2026-04-04 13:56:20 +0100

    desktop page ready

--- app/_components/layout/Header.tsx
+++ app/_components/layout/Header.tsx
@@ -1,27 +1,32 @@
-'use client'
+"use client";
 import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const Header = () => {
-  const param = useParams()
+  const param = useParams();
 
-  const [ isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 20)
-    }
+      setIsScrolled(window.scrollY > 20);
+    };
     window.addEventListener("scroll", handleScroll);
- return () => window.removeEventListener("scroll", handleScroll);
-  }, [])
- 
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className={clsx("px-4 sticky mx-auto z-10 w-full ", 
-      isScrolled ? "bg-black/90 top-4 max-w-6xl w-full rounded-lg text-gray-200 ": "bg-black/10 top-0 text-black"
-    )}>
+    <div
+      className={clsx(
+        "px-4 sticky mx-auto z-10 w-full ",
+        isScrolled
+          ? "bg-black/98 top-4 max-w-6xl w-full rounded-lg text-gray-200 "
+          : "bg-black/10 top-0 text-black",
+      )}
+    >
       <div className="max-w-6xl mx-auto py-2 px-4  flex items-center justify-between h-20  overflow-hidden">
         <div className="">
           <Image
@@ -34,21 +39,21 @@ const Header = () => {
         </div>
         <div className=" flex gap-8 whitespace-nowrap text-sm font-semibold">
           {navItems.map((item, i) => (
-            <Link href={item.url} key={i} >
+            <Link href={item.url} key={i}>
               {item.value}
             </Link>
           ))}
         </div>
 
-          <button className=" w-fit h-10 rounded-lg bg-slate-800 px-8 py-2 text-gray-200 text-sm font-medium">
-            Book now
-          </button>
+        <button className=" w-fit h-10 rounded-lg bg-slate-800 px-8 py-2 text-gray-200 text-sm font-medium">
+          Book now
+        </button>
       </div>
     </div>
   );
 };
 
- export const navItems : {url: string, value: string}[] = [
+export const navItems: { url: string; value: string }[] = [
   {
     url: "/",
     value: "Home",
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import Hero from "../_components/layout/Hero";
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <Hero
+        heading="Contact us"
+        text="The elegant luxury bedrooms in this gallery showcase custom interior designs & decorating ideas. View pictures and find your perfect luxury bedroom design."
+      />
 
-export default page
+      <div>form </div>
+      <div className=""> map</div>
+    </div>
+  );
+};
+
+export default page;
