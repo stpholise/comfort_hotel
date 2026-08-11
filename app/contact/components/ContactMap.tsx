4021a3079862035c38d24177d65e3ef40814a9ad
stephen olise <stpholise@gmail.com>
2026-08-11 01:55:11 +0100

    add a leaflet map to contact page

---
+++ app/contact/components/ContactMap.tsx
@@ -0,0 +1,31 @@
+// import "@maptiler/sdk/dist/maptiler-sdk.css";
+// import { Map, Marker } from "@vis.gl/react-maplibre";
+// import "maplibre-gl/dist/maplibre-gl.css";
+
+// const ContactMap = () => {
+//   const hotel = {
+//     latitude: 6.358165,
+//     longitude: 5.735234,
+//   };
+
+//   return (
+//     <div
+//       style={{ width: "800px", height: "500px" }}
+//       className="w-150 h-130 flex justify-center mx-auto items center"
+//     >
+//       <Map
+//         initialViewState={{
+//           longitude: hotel.longitude,
+//           latitude: hotel.latitude,
+//           zoom: 0,
+//         }}
+//         style={{ width: "100%", height: "100%" }}
+//         mapStyle="https://api.maptiler.com/maps/aquarelle-v4/style.json?key=UrA6O1O1TMOtCWbg2CF2"
+//       >
+//         {/* <Marker longitude={hotel.longitude} latitude={hotel.latitude} /> */}
+//       </Map>
+//     </div>
+//   );
+// };
+
+// export default ContactMap;
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,11 @@
+"use client";
 import Hero from "../_components/layout/Hero";
+import dynamic from "next/dynamic"; 
+
+
+const MyMap = dynamic(() => import("./components/MyMap"), {
+  ssr: false,
+});
 
 const page = () => {
   return (
@@ -7,13 +14,12 @@ const page = () => {
         heading="Contact us"
         text="The elegant luxury bedrooms in this gallery showcase custom interior designs & decorating ideas. View pictures and find your perfect luxury bedroom design."
       />
-
       <div className=" max-w-5xl px-4 py-8 mx-auto">
         <form>
           <div className="  grid grid-cols-2  gap-4  flex-wrap">
             <div className="w-1/2 ">
               <label htmlFor="">Full name</label>
-              <input type="text" className=""/>
+              <input type="text" className="" />
             </div>
             <div className=" w-1/2 ">
               <label htmlFor="">Email</label>
@@ -24,13 +30,14 @@ const page = () => {
               <input type="text" />
             </div>
           </div>
-            <div className="w-full mt-4">
-              <label htmlFor="">Message</label>
-              <input type="text"  className=""/>
-            </div>
+          <div className="w-full mt-4">
+            <label htmlFor="">Message</label>
+            <input type="text" className="" />
+          </div>
         </form>
       </div>
-      <div className=""> map</div>
+    
+      <MyMap />
     </div>
   );
 };
